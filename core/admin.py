2857ae1ca5ae197f055b0db9fbb4eb776d14f5d7
Dolly Kukreja <dolly@glideinvest.com>
2023-04-08 00:48:55 +0530

    added migrations

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from core.models import CustomUser, Address, OneTimePassword
+from core.models import (
+    CustomUser,
+    Address,
+    OneTimePassword,
+    Product,
+    FriendRequestModel,
+)
 
 # Register your models here.
 
 admin.site.register(CustomUser)
 admin.site.register(Address)
 admin.site.register(OneTimePassword)
+admin.site.register(Product)
+admin.site.register(FriendRequestModel)
