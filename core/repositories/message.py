ba4605c3ae254f91499ec43e87decdb7e943aa99
Bhavana Mishra <mishra.a.bhavana@gmail.com>
2023-04-19 08:19:03 +0000

    Fe reported issues

--- core/repositories/message.py
+++ core/repositories/message.py
@@ -4,9 +4,9 @@ from django.db.models import QuerySet, Q
 
 from core.helpers.decorators import handle_unknown_exception
 from core.helpers.query_search import get_or_none
-from core.models import CustomUser, Message
+from core.models import CustomUser, Message, Friends
 from core.serializers.root_serializers import MessageSerializer
-
+from ast import literal_eval
 LOGGER = logging.getLogger(__name__)
 
 
@@ -27,6 +27,15 @@ class MessageRepository:
             item_list=item_list,
             **kwargs,
         )
+
+    @staticmethod
+    @handle_unknown_exception(logger=LOGGER)
+    def get_unread_count(sender, receiver):
+        message_list_object = Message.objects.filter(
+            sender=sender,
+            receiver=receiver,
+            is_read=False).count()
+        return message_list_object
 
     @staticmethod
     @handle_unknown_exception(logger=LOGGER)
@@ -68,6 +77,62 @@ class MessageRepository:
                     receiver=sender_user,
                 )
             ).all()
+            if not message_list_object:
+                return True, "No messages found."
             message_list_object.filter(receiver=sender_user).update(is_read=True)
             message_data = MessageSerializer(message_list_object, many=True).data
             return True, message_data
+
+    @staticmethod
+    @handle_unknown_exception(logger=LOGGER)
+    def get_last_message(current_user):
+        friend_list_object = Friends.objects.filter(user=current_user).first()
+        if not friend_list_object or not friend_list_object.friends_list:
+            return True, "No Friends Found."
+        message_data = []
+        friends_list = literal_eval(friend_list_object.friends_list)
+        for friend in friends_list:
+            friend_object = get_or_none(CustomUser, user_id=friend)
+            message_list_object = Message.objects.filter(
+                Q(
+                    sender=current_user,
+                    receiver=friend_object,
+                )
+                | Q(
+                    sender=friend_object,
+                    receiver=current_user,
+                )
+            ).order_by('-created_date').first()
+            unread_count = 0
+            if not message_list_object:
+                message_data.append(
+                    {
+                        "friend_name": friend_object.full_name,
+                        "photo": str(friend_object.profile_photo.url)
+                        if friend_object.profile_photo
+                        else None,
+                        "user_id": friend_object.user_id,
+                        "last_message": "",
+                        "timestamp": "",
+                        "unread_count": unread_count
+                    }
+                )
+            else:
+                if message_list_object.receiver == current_user:
+                    unread_count = MessageRepository.get_unread_count(sender=friend_object, receiver=current_user)
+                message_data.append(
+                    {
+                        "friend_name": friend_object.full_name,
+                        "photo": str(friend_object.profile_photo.url)
+                        if friend_object.profile_photo
+                        else None,
+                        "user_id": friend_object.user_id,
+                        "last_message": message_list_object.message,
+                        "timestamp": message_list_object.created_date,
+                        "unread_count": unread_count
+                    }
+                )
+        return True, message_data
+
+
+
